7bd704b5c4c749b1fceee2bffe2a094e2d78bd2f
Michael Andersen <michael@steelcode.com>
2015-02-02 10:29:55 -0800

    Update echo

--- app/echo.lua
+++ app/echo.lua
@@ -11,10 +11,9 @@ brd = LED:new("GP0")
 
 print("echo test")
 brd:flash(4)
-sock = nil
 
 ipaddr = storm.os.getipaddr()
-ipaddrs = string.format("%04x%04x:%04x%04x:%04x%04x:%04x%04x::%04x%04x:%04x%04x:%04x%04x:%04x%04x",
+ipaddrs = string.format("%02x%02x:%02x%02x:%02x%02x:%02x%02x::%02x%02x:%02x%02x:%02x%02x:%02x%02x",
 			ipaddr[0],
 			ipaddr[1],ipaddr[2],ipaddr[3],ipaddr[4],
 			ipaddr[5],ipaddr[6],ipaddr[7],ipaddr[8],	
@@ -23,51 +22,51 @@ ipaddrs = string.format("%04x%04x:%04x%04x:%04x%04x:%04x%04x::%04x%04x:%04x%04x:
 
 print("ip addr", ipaddrs)
 print("node id", storm.os.nodeid())
+cport = 49152
 
 -- create echo server as handler
 server = function()
-   sock = storm.net.udpsocket(7, 
-			      function(payload, from, port)
-				 brd:flash(1)
-				 print (string.format("from %s port %d: %s",from,port,payload))
--- client should be able to listen on the port that this arrives on
--- print(storm.net.sendto(sock, payload, from, port))
-				 print(storm.net.sendto(sock, payload, from, 7))
-				 brd:flash(1)
-			      end)
-end
-
--- client side
-button = require("button")
-btn = button:new("D9")		-- button 1 on starter shield
-
-client = function()
-   blu = LED:new("D2")		-- LEDS on starter shield
-   grn = LED:new("D3")
-   red = LED:new("D4")
-   -- replace server handler with response handler
-   sock = storm.net.udpsocket(7, 
-			      function(payload, from, port)
-				 red:flash(3)
-				 print (string.format("echo from %s port %d: %s",from,port,payload))
-			      end)
-   -- send echo on each button press
-   count = 0
---   btn:whenever("FALLING",
-   storm.os.invokePeriodically(5*storm.os.SECOND,
-			       function() 
-				  blu:flash(1)
-				  local msg = string.format("0x%04x says count=%d", storm.os.nodeid(), count)
-				  print(msg)
-				  -- end upd echo to link local all nodes multicast
-				  storm.net.sendto(sock, msg, "ff02::1", 7) 
-				  count = count + 1
-				  grn:flash(1)
+   ssock = storm.net.udpsocket(7, 
+			       function(payload, from, port)
+				  brd:flash(1)
+				  print (string.format("from %s port %d: %s",from,port,payload))
+				  print(storm.net.sendto(ssock, payload, from, cport))
+				  brd:flash(1)
 			       end)
 end
 
+server()			-- every node runs the echo server
 
---btn:when("CHANGE",client)
+-- client side
+Button = require("button")
+btn1 = Button:new("D9")		-- button 1 on starter shield
+blu = LED:new("D2")		-- LEDS on starter shield
+grn = LED:new("D3")
+red = LED:new("D4")
+count = 0
+-- create client socket
+csock = storm.net.udpsocket(cport, 
+			    function(payload, from, port)
+			       red:flash(3)
+			       print (string.format("echo from %s port %d: %s",from,port,payload))
+			    end)
+
+-- send echo on each button press
+client = function()
+   blu:flash(1)
+   local msg = string.format("0x%04x says count=%d", storm.os.nodeid(), count)
+   print("send:", msg)
+   -- send upd echo to link local all nodes multicast
+   storm.net.sendto(csock, msg, "ff02::1", 7) 
+   count = count + 1
+   grn:flash(1)
+end
+
+-- button press runs client
+btn1:whenever("RISING",function() 
+		print("Run client")
+		client() 
+		      end)
 
 -- enable a shell
 sh = require "stormsh"
