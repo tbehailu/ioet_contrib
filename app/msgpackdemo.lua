69458f01e6ad7dabb96f3bbc10a3f48831ac896f
Gabe Fierro <gtfierro225@gmail.com>
2015-02-04 09:42:22 -0800

    add msgpack demo

---
+++ app/msgpackdemo.lua
@@ -0,0 +1,26 @@
+-- This script serializes and then unserializes a table
+
+-- create our data
+local t = {}
+t.a = "abc"
+t.b = 123
+t.c = {1,2,3}
+t.d = {}
+t.d["a"] = 7
+t.d["b"] = 8
+t.d["c"] = 9
+print("packing", t)
+
+-- call storm.mp.pack with the target as the argument. The target
+-- can be a table, array, string, number or stormarray
+p = storm.mp.pack(t)
+print("packed:", p)
+
+-- unpack a serialized target
+up = storm.mp.unpack(p)
+print("should be abc", t.a)
+print("should be 123", t.b)
+print("should be {1,2,3}", t.c)
+for idx,val in ipairs(t.c) do print("index",idx,"value",val) end
+print("should be table", t.d)
+for key,val in pairs(t.d) do print("key",key,"value",val) end
